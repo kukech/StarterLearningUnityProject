41e6d0186288d2ce934ecff1ecd5a72ff330959e
kukech <qwadik@gmail.com>
2021-02-17 16:23:12 +0300

    update: scene1_4: i learned how to work with texture, materials, models, particles, geometry model as first step in designing scene

---
+++ Assets/Scripts/BackAndForth.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackAndForth : MonoBehaviour
+{
+    public float speed = 3.0f;
+    public float maxX = 20.0f; 
+    public float minX = -2f;
+    private int _direction = 1; 
+    void Update()
+    {
+        transform.Translate(_direction * speed * Time.deltaTime, 0, 0);
+
+        bool bounced = false;
+        if (transform.position.x > maxX || transform.position.x < minX)
+        {
+            _direction = -_direction; 
+            bounced = true;
+        }
+        if (bounced)
+        {
+            transform.Translate(_direction * speed * Time.deltaTime, 0, 0);
+        }
+    }
+
+}
